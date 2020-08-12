8c829c3df9a5c0acba0eae1d3299bbfc8a92660d
tangyuzhe <38202539+tangyuzhe@users.noreply.github.com>
2020-08-12 13:42:54 +0800

    去除查询个人信息的权限验证

--- app/router/alumnus.js
+++ app/router/alumnus.js
@@ -2,7 +2,7 @@ module.exports = app => {
   const { router, controller, jwt } = app;
   const alumnu = app.controller.alumnus
   router.post('/api/alumnus/createAlumnu', jwt, alumnu.createAlumnu)
-  router.get('/api/alumnus/findAlumnu', jwt, alumnu.findAlumnu)
+  // router.get('/api/alumnus/findAlumnu', jwt, alumnu.findAlumnu)
   router.delete('/api/alumnus/:id', jwt, alumnu.deleteAlumnu)
   router.put('/api/alumnus', jwt, alumnu.updateAlumnu)
   router.get('/api/alumnus', jwt, alumnu.findAll)
