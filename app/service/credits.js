2f2b8ef0742d1a05edf202ba0c2431fe9ab31890
Dalin-liang <1409939929@qq.com>
2020-09-04 15:00:49 +0800

    修改接口

--- app/service/credits.js
+++ app/service/credits.js
@@ -9,7 +9,7 @@ class CreditsService extends Service {
      */
   async findCredits(sno) {
     const { ctx } = this;
-    const res = await ctx.model.Credits.findOne({
+    const res = await ctx.model.Credits.findAll({
     where: {
         sno: sno
     }
