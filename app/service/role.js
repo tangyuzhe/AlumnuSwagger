0d99250475eeb161f26519cdf2d6b7833353f03e
Dalin-liang <1409939929@qq.com>
2020-10-18 09:44:08 +0800

    Role角色表：在用户完成绑定后才能进行数据添加，在绑定时需要先查询是否已经存在

--- app/service/role.js
+++ app/service/role.js
@@ -24,29 +24,33 @@ class RoleService extends Service {
     }
   }
 
-
   async updateOpenid(userid, openid) {
     const { ctx } = this;
-    const res = await ctx.model.Role.update({
-      openid: openid
-    }, {
+    const mark = await ctx.model.Role.findOne({
       where: {
         userid: userid
       }
     })
-    if (res == 0) {
-      ctx.throw(404, { code: 1, message: "修改失败" })
+    if (!mark) {
+      ctx.throw(404, { code: 1, message: "查无此数据" })
     } else {
-      return {
-        code: 0,
-        message: "修改成功"
+      const res = await ctx.model.Role.update({
+        openid: openid
+      }, {
+        where: {
+          userid: userid
+        }
+      })
+      if (res == 0) {
+        ctx.throw(404, { code: 1, message: "修改失败" })
+      } else {
+        return {
+          code: 0,
+          message: "修改成功"
+        }
       }
-    }
+    }    
   }
-
-
-
-
 }
 
 module.exports = RoleService;
